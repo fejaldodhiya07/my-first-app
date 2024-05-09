2b5ad1eb621e4c74721d082fa5972f6854fa33ec
Fejal Dodhiya <dodhiyafejal07@gmail.com>
2024-05-09 18:59:50 +0530

    Updates

---
+++ precache-manifest.7f7ee5a9273e436c55087cbd2c432822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e003d58eeab1b7ff2cad91518cdb8928",
+    "url": "/my-first-app/index.html"
+  },
+  {
+    "revision": "fdf80ce608e593226443",
+    "url": "/my-first-app/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/my-first-app/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my-first-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "fdf80ce608e593226443",
+    "url": "/my-first-app/static/js/main.dd923562.chunk.js"
+  },
+  {
+    "revision": "d82c20c3a4fbb92c4b09",
+    "url": "/my-first-app/static/js/runtime~main.ed2a2c0c.js"
+  }
+]);
